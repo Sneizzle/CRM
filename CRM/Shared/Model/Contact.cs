5625baa9ad11a55cfaf88afecf261d1fe45f63ed
Sigurd W <sigurd_w@hotmail.com>
2023-10-27 12:39:56 +0200

    Model klasserne "Customer" og "Contact" oprettet

---
+++ CRM/Shared/Model/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Shared.Model
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsHidden { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+    }
+}
---
+++ CRM/Shared/Model/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Shared.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? StreetName { get; set; }
+        public string? ZipCode { get; set; }
+        public string? CityName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int? ServiceHours { get; set; }
+        public string CVR { get; set; }
+        public string EAN { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
