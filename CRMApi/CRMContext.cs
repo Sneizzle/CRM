d6e6e1e58dace7a84fac91cbacd7f87f82070a0d
Sigurd W <sigurd_w@hotmail.com>
2023-10-30 09:57:54 +0100

    Controller til "Customer" og "Contact"

---
+++ CRMApi/CRMContext.cs
@@ -0,0 +1,17 @@
+﻿using CRM.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CRMApi
+{
+    public class CRMContext : DbContext
+    {
+        public CRMContext(DbContextOptions<CRMContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
