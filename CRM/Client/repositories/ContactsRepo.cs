072367b324104e511cef8da03cde8175765c0d39
Tobiasdanielsen123115 <Tobiasdanielsen35@gmail.com>
2023-11-09 13:07:45 +0100

    Repo folders, NOT AT ALL DONE,

---
+++ CRM/Client/repositories/ContactsRepo.cs
@@ -0,0 +1,6 @@
+﻿namespace CRM.Client.repositories
+{
+    public class ContactsRepo
+    {
+    }
+}
---
+++ CRM/Client/repositories/CustomerRepo.cs
@@ -0,0 +1,27 @@
+﻿using CRM.Shared.Model;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+
+
+namespace CRM.Client.repositories
+{
+    public class CustomerRepo
+    {
+        static HttpClient http = new HttpClient();
+
+        private List<Customer> customers;
+
+        private List<Contact> contacts;
+
+        protected async Task gottaGetThemAll()
+        {
+
+            customers = await http.GetFromJsonAsync<List<Customer>>("api/Customers");
+            contacts = await http.GetFromJsonAsync<List<Contact>>("api/Contacts");
+
+
+        }
+
+    }
+}
