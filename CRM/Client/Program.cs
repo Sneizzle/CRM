dedf6c0ac1be294276146dd701810b6e2f48c447
Emil9 <enab38868@edu.ucl.dk>
2023-11-13 09:57:44 +0100

    repo wip

--- CRM/Client/Program.cs
+++ CRM/Client/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.Modal;
 using CRM.Client;
+using CRM.Client.repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,6 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredModal();
+//builder.Services.AddScoped<CustomerRepo>();
 
 builder.Services.AddHttpClient("CRM.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
--- CRM/Client/repositories/CustomerRepo.cs
+++ CRM/Client/repositories/CustomerRepo.cs
@@ -10,22 +10,39 @@ namespace CRM.Client.repositories
     public class CustomerRepo
     {
         static HttpClient http = new HttpClient();
+        private static CustomerRepo instance;
+        public static CustomerRepo Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CustomerRepo();
+                }
+                return instance;
+            }
+            //set { _instance = value; }
+        }
+        public List<Customer> customers;
+        private CustomerRepo()
+        {
+            //customers = await http.GetFromJsonAsync<List<Customer>>("api/Customers");
+            customers = new List<Customer>
+            {
+            new Customer() { Name = "test"},
+            new Customer() { Name = "name2"}
+            };
+        }
 
-        public List<Customer> customers = new List<Customer>();
         public List<Customer> GetList()
         {
             return customers;
         }
 
-        public List<Contact> contacts;
 
-      
-        protected async Task gottaGetThemAll()
+        public async Task gottaGetThemAll()
         {
             customers = await http.GetFromJsonAsync<List<Customer>>("api/Customers");
-            contacts = await http.GetFromJsonAsync<List<Contact>>("api/Contacts");
-     
         }
-
     }
 }
