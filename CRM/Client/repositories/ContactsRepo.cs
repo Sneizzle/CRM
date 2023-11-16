bb2177128b8241ff8a1dc52a82072a5d27220b20
Emil9 <enab38868@edu.ucl.dk>
2023-11-16 11:53:08 +0100

    Contact repo

--- CRM/Client/repositories/ContactsRepo.cs
+++ CRM/Client/repositories/ContactsRepo.cs
@@ -1,6 +1,54 @@
-﻿namespace CRM.Client.repositories
+﻿using CRM.Client.Repositories;
+using CRM.Shared.Model;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+
+namespace CRM.Client.repositories
 {
     public class ContactsRepo
     {
+        static HttpClient http = new HttpClient()
+        {
+            BaseAddress = new Uri("https://localhost:7047/api")
+        };
+
+        private static CustomerRepo instance;
+        public static CustomerRepo Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CustomerRepo();
+                }
+                return instance;
+            }
+            set { instance = value; }
+        }
+
+        public List<Contact> contacts;
+
+        public ContactsRepo()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public async Task InitializeContacts()
+        {
+            try
+            {
+                contacts = await http.GetFromJsonAsync<List<Contact>>("api/Contacts");
+            }
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync("InitializeContacts error: " + e);
+            }
+        }
+
+        public void AddToContacts(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
     }
 }
