8ea83a0f8ba1d26c49161195ad56c4f94a517e45
Sigurd W <sigurd_w@hotmail.com>
2023-11-20 10:54:29 +0100

    Added controllers for ServiceHours and Assignments

---
+++ CRM/Server/Data/Migrations/20231120095329_addedAssignmentsAndServiceHours.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedAssignmentsAndServiceHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Customers_customerId",
+                table: "Tasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "Assignments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_customerId",
+                table: "Assignments",
+                newName: "IX_Assignments_customerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Customers_customerId",
+                table: "Assignments",
+                column: "customerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Customers_customerId",
+                table: "Assignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments");
+
+            migrationBuilder.RenameTable(
+                name: "Assignments",
+                newName: "Tasks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Assignments_customerId",
+                table: "Tasks",
+                newName: "IX_Tasks_customerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Customers_customerId",
+                table: "Tasks",
+                column: "customerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
